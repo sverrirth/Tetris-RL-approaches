9a4d292b8ff264741a1a9a1bf0bccb86fe2aeec7
Max Morin <morin.max@gmail.com>
2015-05-05 08:23:29 +0200

    Code format and minor cleanup. Directory structure. Added gitignore.

--- smalltetris/State.java
+++ smalltetris/State.java
@@ -1,34 +1,35 @@
-package tetris;
+package smalltetris;
 
 public final class State {
-	
+
 	private final boolean[][] well;
 	private final int rowsRemoved;
-	
+
 	public State(boolean[][] w, int r) {
 		well = w;
 		rowsRemoved = r;
 	}
-	
+
 	public boolean[][] getWell() {
 		return well;
 	}
-	
+
 	public int getRowsRemoved() {
 		return rowsRemoved;
 	}
-	
+
+	@Override
 	public String toString() { //just for debugging
 		String ret = "";
-		for(int i=1;i>-1;i--) {
-			for(int j=0;j<5;j++) {
-				if (well[j][i]) {
+		for(int i = 1; i > -1; i--) {
+			for(int j = 0; j < 6; j++) {
+				if(well[j][i]) {
 					ret += "X ";
-				} else ret += "_ ";
+				} else {
+					ret += "_ ";
+				}
 			}
-			if (well[5][i]) {
-				ret += "X " + "\n";
-			} else ret += "_ " + "\n";
+			ret += "\n";
 		}
 		return ret;
 	}
